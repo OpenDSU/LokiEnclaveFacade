db6ac19acef0dd34ddbfa89964c758fc039cb29d
skutner <sava.dumitru.daniel@gmail.com>
2024-06-06 15:53:05 +0300

    Add test for removeCollection functionality
    This commit introduces a test file for the 'removeCollection' function within the LokiPartitioningAdapter. It includes the creation and removal of collections in a newly created Loki instance, asserting post-removal to ensure that the specified collection no longer exists in the Database.

---
+++ tests/removeCollectionTest.js
@@ -0,0 +1,61 @@
+require(" ../../../builds/output/testsRuntime");
+const loki = require('../lib/lokijs/src/lokijs');
+const LokiPartitioningAdapter = loki.LokiPartitioningAdapter;
+const lokiPartitioningAdapter = new LokiPartitioningAdapter(new loki.LokiFsAdapter());
+const fs = require('fs');
+const path = require('path');
+const assert = require('double-check').assert;
+
+assert.callback("Test removeCollection function with LokiPartitioningAdapter", async (testFinishCallback) => {
+    // create a test folder
+    const folder = './testFolder';
+    if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder, {recursive: true});
+    }
+    const DB_FILEPATH = path.join(folder, 'test.db')
+
+    // Create a new database with LokiPartitioningAdapter
+    const db = new loki(DB_FILEPATH, {adapter: lokiPartitioningAdapter});
+
+    // Create multiple collections
+    const collectionNames = ['testCollection1', 'testCollection2', 'testCollection3'];
+    collectionNames.forEach(name => db.addCollection(name));
+
+    // Save the database
+    await new Promise((resolve, reject) => {
+        db.saveDatabase((err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+
+    // Remove a collection
+    const removedCollectionName = collectionNames[collectionNames.length - 1];
+    db.removeCollection(removedCollectionName);
+
+    // Save the database again
+    await new Promise((resolve, reject) => {
+        db.saveDatabase((err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+
+    const newLokiInstance = new loki(DB_FILEPATH, {adapter: lokiPartitioningAdapter});
+    // Load the database
+    await new Promise((resolve, reject) => {
+        newLokiInstance.loadDatabase({}, (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+
+    // Check if the removed collection is still present in the database
+    const removedCollection = newLokiInstance.getCollection(removedCollectionName);
+    assert.equal(removedCollection, null);
+
+    // remove the test folder
+    fs.rmSync(folder, {recursive: true});
+
+    testFinishCallback();
+});
