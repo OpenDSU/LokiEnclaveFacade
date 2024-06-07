239e8da7338620f0c76d61729e18e66f18171161
skutner <sava.dumitru.daniel@gmail.com>
2024-06-07 10:17:48 +0300

    Update test file to use async database operations
    The database operations for saving and loading the database in the `removeCollectionTest.js` test file have been updated. They are now implemented and used as asynchronous functions for cleaner and more readable code. Also, the database file extension has been changed to '.json' from '.db'.

--- tests/removeCollectionTest.js
+++ tests/removeCollectionTest.js
@@ -12,43 +12,44 @@ assert.callback("Test removeCollection function with LokiPartitioningAdapter", a
     if (!fs.existsSync(folder)) {
         fs.mkdirSync(folder, {recursive: true});
     }
-    const DB_FILEPATH = path.join(folder, 'test.db')
+    const DB_FILEPATH = path.join(folder, 'test.db.json')
 
     // Create a new database with LokiPartitioningAdapter
     const db = new loki(DB_FILEPATH, {adapter: lokiPartitioningAdapter});
+    const saveDatabaseAsync = () => {
+        return new Promise((resolve, reject) => {
+            db.saveDatabase((err) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    };
 
+    const loadDatabaseAsync = () => {
+        return new Promise((resolve, reject) => {
+            db.loadDatabase({}, (err) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+    }
     // Create multiple collections
     const collectionNames = ['testCollection1', 'testCollection2', 'testCollection3'];
     collectionNames.forEach(name => db.addCollection(name));
 
     // Save the database
-    await new Promise((resolve, reject) => {
-        db.saveDatabase((err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
+    await saveDatabaseAsync();
 
     // Remove a collection
     const removedCollectionName = collectionNames[collectionNames.length - 1];
     db.removeCollection(removedCollectionName);
 
     // Save the database again
-    await new Promise((resolve, reject) => {
-        db.saveDatabase((err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
+    await saveDatabaseAsync();
 
     const newLokiInstance = new loki(DB_FILEPATH, {adapter: lokiPartitioningAdapter});
     // Load the database
-    await new Promise((resolve, reject) => {
-        newLokiInstance.loadDatabase({}, (err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
+    await loadDatabaseAsync();
 
     // Check if the removed collection is still present in the database
     const removedCollection = newLokiInstance.getCollection(removedCollectionName);
