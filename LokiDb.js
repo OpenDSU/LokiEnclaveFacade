ca97aa952e469518eac05aa6bac5452606c8886b
skutner <sava.dumitru.daniel@gmail.com>
2025-01-09 14:42:45 +0200

    Use partitioned adapter as default for LokiDb initialization.
    Replaced the default file system (FS) adapter with the partitioned adapter to improve data handling. This change ensures better scalability and alignment with updated storage requirements. No other functionality is affected.

--- LokiDb.js
+++ LokiDb.js
@@ -23,8 +23,7 @@ function LokiDb(rootFolder, autosaveInterval, adaptorConstructorFunction) {
     const SEED_SSIS_TABLE = "seedssis";
     const DIDS_PRIVATE_KEYS = "dids_private";
     const AUTOSAVE_INTERVAL = 5000;
-    const adapter = adaptorConstructorFunction === undefined ? new Adapters.FS() : new adaptorConstructorFunction();
-
+    const adapter = adaptorConstructorFunction === undefined ? new Adapters.PARTITIONED() : new adaptorConstructorFunction();
     logger.info(`Initializing Loki database ${rootFolder}`);
     autosaveInterval = autosaveInterval || AUTOSAVE_INTERVAL;
     if (typeof rootFolder === "undefined") {
